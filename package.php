21425f13bf9653d086128086806a0ef3b5a5088e
Aaron Holbrook <AaronHolbrook@users.noreply.github.com>
2018-08-30 07:14:37 -0500

    Update package.php

--- package.php
+++ package.php
@@ -2,11 +2,8 @@
 
 namespace Zeek\Icons;
 
-use function A7\autoload;
-
 define( 'ICONS_VERSION', '1.0.0' );
-define( 'ICONS_URL', plugin_dir_url( __FILE__ ) );
 define( 'ICONS_PATH', dirname( __FILE__ ) . '/' );
-define( 'ICONS_TEMPLATES', FRONTEND_ACCOUNT_PATH . 'templates/' );
+define( 'ICONS_TEMPLATES', ICONS_PATH . 'templates/' );
 
 require_once __DIR__ . '/src/main.php';
